0f8724d08e7ce4e91ebcd59198de866b9df0c156
Görkem Kethüda <gorkemkethuda@gmail.com>
2022-01-03 21:46:28 +0300

    Create navigation structure with bottom navigation bar

---
+++ app/src/main/java/com/gketdev/xkcdreader/presentation/FavoriteFragment.kt
@@ -0,0 +1,18 @@
+package com.gketdev.xkcdreader.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.gketdev.xkcdreader.base.BaseFragment
+import com.gketdev.xkcdreader.databinding.FragmentFavoriteBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>() {
+    override fun initViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        attachToParent: Boolean
+    ): FragmentFavoriteBinding {
+        return FragmentFavoriteBinding.inflate(inflater, container, attachToParent)
+    }
+}
---
+++ app/src/main/java/com/gketdev/xkcdreader/presentation/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.gketdev.xkcdreader.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gketdev.xkcdreader.base.BaseFragment
+import com.gketdev.xkcdreader.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override fun initViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        attachToParent: Boolean
+    ): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(inflater, container, attachToParent)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
